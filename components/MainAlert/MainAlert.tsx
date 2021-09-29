81a4578f00e6ec5b333596d47c552d60e803f636
Areski Himeur <areski.himeur@gmail.com>
2021-09-29 12:52:04 +0200

    :bug: Use marked instead of react-markdown

--- components/MainAlert/MainAlert.tsx
+++ components/MainAlert/MainAlert.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from './MainAlert.module.css'
-import ReactMarkdown from "react-markdown";
+import marked from 'marked';
+
 
 type PropsMainAlert = { message: string, boxColor: string }
 
 export const MainAlert = ({ message, boxColor }: PropsMainAlert) => (
     <div className={styles.alertBoxMessage} style={{ backgroundColor: boxColor }}>
         <p className={styles.alertTextMessage}>
-            <ReactMarkdown escapeHtml={true} source={message} />
+            <div dangerouslySetInnerHTML={{ __html: marked(message, {sanitize: true})}} />
         </p>
     </div>
 );
