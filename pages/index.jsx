c1f8199666ce01d9b4703426577ca34b0bc5ad88
Areski Himeur <areski.himeur@gmail.com>
2022-03-01 17:50:46 +0100

    :rocket: Update cards with CMS

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,42 +5,51 @@ import MainAlert from "../components/MainAlert/MainAlert";
 import Card from "../components/Card/Card";
 import Footer from "../components/Footer/Footer";
 import styles from "./Home.module.css";
-import { request } from "../lib/datocms";
+import {request} from "../lib/datocms";
 
-const MAINALERT_QUERY = `query MyQuery { mainAlert { message, boxColor { alpha, blue, green, red } }}`;
+const MAINALERT_QUERY = `query HomeQuery{allHomeCards{id, link, short, title}, mainAlert{on, message, boxColor {alpha, blue, green, red}}}`;
 
 export async function getStaticProps() {
     try {
-        const alertProps = await request({ query: MAINALERT_QUERY });
+        const alertProps = await request({query: MAINALERT_QUERY});
         const alertMessage = alertProps.mainAlert.message
+        const alertOn = alertProps.mainAlert.on
         const alertBoxColor = "rgb("
             + alertProps.mainAlert.boxColor.red + ","
             + alertProps.mainAlert.boxColor.green + ","
             + alertProps.mainAlert.boxColor.blue + ","
             + "." + alertProps.mainAlert.boxColor.alpha + ")"
-        return { props: { alertMessage, alertBoxColor } };
+        const cardList = alertProps.allHomeCards.map(card => {
+            return {
+                id: card.id,
+                link: card.link,
+                short: card.short,
+                title: card.title
+            }
+        })
+        return {props: {alertOn, alertMessage, alertBoxColor, cardList}};
     } catch (error) {
         const alertMessage = "Error while catching alert message."
         const alertBoxColor = "#CC006620"
-        return { props: { alertMessage, alertBoxColor } };
+        return {props: {alertMessage, alertBoxColor}};
     }
 }
 
-export default function Home({ alertMessage, alertBoxColor }) {
+export default function Home({alertOn, alertMessage, alertBoxColor, cardList}) {
     return (
         <div className={styles.container}>
             <Head>
                 <title>Areski Guilhem Himeur - Curriculum vitæ</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet" />
-                <meta name="description" content="Étudiant en Informatique Théorique à l'université de Montpellier" />
-                <meta property="og:type" content="website" />
-                <meta name="og:title" property="og:title" content="Areski Guilhem Himeur - Curriculum vitæ" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet"/>
+                <meta name="description" content="Étudiant en Informatique Théorique à l'université de Montpellier"/>
+                <meta property="og:type" content="website"/>
+                <meta name="og:title" property="og:title" content="Areski Guilhem Himeur - Curriculum vitæ"/>
                 <meta name="og:description" property="og:description"
                       content="Doctorant en Informatique Théorique à l'université de Montpellier"/>
             </Head>
@@ -53,28 +62,16 @@ export default function Home({ alertMessage, alertBoxColor }) {
                     "intelligence artificielle, optimisation, théorie des graphes, calculabilité, complexité, " +
                     "algorithmique probabiliste, etc."}
                 />
-                <MainAlert message={alertMessage} boxColor={alertBoxColor} ></MainAlert>
+                {alertOn && <MainAlert message={alertMessage} boxColor={alertBoxColor}></MainAlert>}
                 <div className={styles.grid}>
-                    <Card
-                        title={"Curriculum vitæ"}
-                        link={"./CV - Areski Guilhem Himeur.pdf"}
-                        description={"Une version PDF de mon Curriculum vitæ"}
-                    />
-                    <Card
-                        title={"Contact"}
-                        link={"mailto:himeur@areski.info"}
-                        description={"Contactez moi sur himeur@areski.info"}
-                    />
-                    <Card
-                        title={"Linkedin"}
-                        link={"https://www.linkedin.com/in/AreskiHimeur/"}
-                        description={"Plus de détails sur mon profil Linkedin"}
-                    />
-                    <Card
-                        title={"Github"}
-                        link={"https://github.com/Hareski"}
-                        description={"Quelques projets sur mon compte Github"}
-                    />
+                    {cardList.map(card => (
+                        <Card
+                            key={card.id}
+                            link={card.link}
+                            description={card.short}
+                            title={card.title}
+                        />
+                    ))}
                 </div>
             </main>
             <Footer
@@ -85,16 +82,19 @@ export default function Home({ alertMessage, alertBoxColor }) {
             />
 
             <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * { box-sizing: border-box; }
-      `}</style>
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+              }
+
+              * {
+                box-sizing: border-box;
+              }
+            `}</style>
         </div>
     )
 }
